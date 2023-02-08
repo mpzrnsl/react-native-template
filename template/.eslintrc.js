3d8a3f2742700ddbbfbfdff6638f0e3160a0b116
Phia Nasol <mpzrnsl@outlook.com>
2023-02-08 16:44:15 +0800

    Configure eslint and prettier

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: "@react-native-community",
+  rules: {
+    "comma-dangle": [1, "never"],
+    quotes: [1, "double", "avoid-escape"],
+    "no-console": 2
+  }
 };
