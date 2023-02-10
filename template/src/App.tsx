244ad1336121df85b3e0e0a842da61f57cab8ab7
Phia Nasol <mpzrnsl@outlook.com>
2023-02-11 04:21:34 +0800

    Add error boundary

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -10,17 +10,24 @@
 import React from "react";
 import { Text } from "react-native";
 
-import { Env, persistor, store } from "@core";
+import { Env, Logger, persistor, store } from "@core";
+import ErrorBoundary from "react-native-error-boundary";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
+const onErrorHandler = (error: Error, stackTrace: string) => {
+  Logger.extend("SYSTEM").error(error, stackTrace);
+};
+
 function App() {
   return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <Text>{Env.get("ENV")}</Text>
-      </PersistGate>
-    </Provider>
+    <ErrorBoundary onError={onErrorHandler}>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Text>{Env.get("ENV")}</Text>
+        </PersistGate>
+      </Provider>
+    </ErrorBoundary>
   );
 }
 
