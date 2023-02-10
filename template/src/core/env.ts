ea2e9000b486952e7da5f7272053fb0051acc9ba
Phia Nasol <mpzrnsl@outlook.com>
2023-02-10 16:31:52 +0800

    Add env helper

--- template/src/core/env.ts
+++ template/src/core/env.ts
@@ -3,10 +3,48 @@
  *
  * Env
  *
- * Author:
- * Company:
- *
+ * Author: Phia Nasol
  *
  */
 
-export default {};
+import Config from "react-native-config";
+
+interface IEnv {
+  keys(): string[];
+
+  hasKey(key: string): boolean;
+
+  get(key: string, fallback?: string): string;
+}
+
+const Env: IEnv = {
+  keys: function () {
+    return Object.keys(Config);
+  },
+
+  hasKey: function (key) {
+    return this.keys().includes(key);
+  },
+
+  get: function (key, fallback) {
+    if (!this.hasKey(key)) {
+      throw new Error(`The environment variables does not contain key: ${key}`);
+    }
+
+    const value = Config[key];
+
+    if (!value) {
+      if (!fallback) {
+        throw new Error(
+          `The environment variable fallback for key: ${key} is not defined.`
+        );
+      }
+
+      return fallback;
+    }
+
+    return value;
+  }
+};
+
+export default Env;
