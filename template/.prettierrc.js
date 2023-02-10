3617ffa65194a38c0ba6e01572c60f806798439f
Phia Nasol <mpzrnsl@outlook.com>
2023-02-11 03:03:45 +0800

    Configure sort imports

--- template/.prettierrc.js
+++ template/.prettierrc.js
@@ -3,5 +3,17 @@ module.exports = {
   bracketSameLine: true,
   bracketSpacing: true,
   singleQuote: false,
-  trailingComma: "none"
+  trailingComma: "none",
+  importOrder: [
+    "^\\u0000",
+    "^(react$)|^(react-native$)",
+    "<THIRD_PARTY_MODULES>",
+    "^[./]"
+  ],
+  importOrderBuiltinModulesToTop: true,
+  importOrderCaseInsensitive: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderMergeDuplicateImports: true,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true
 };
