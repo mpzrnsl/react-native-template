2b56a664e7c2dca6c888b4746cf222047631cf77
Phia Nasol <mpzrnsl@outlook.com>
2023-02-11 07:59:55 +0800

    Setup i18n

--- template/src/core/i18n/index.ts
+++ template/src/core/i18n/index.ts
@@ -3,10 +3,34 @@
  *
  * i18n
  *
- * Author:
- * Company:
- *
+ * Author: Phia Nasol
  *
  */
+import "./polyfills";
 
-export default {};
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import { Locales } from "@core/assets";
+import Env from "@core/env";
+
+import languageDetector from "./languageDetector";
+
+export const defaultNS = "translation";
+
+export const resources = {
+  en: Locales.en
+} as const;
+
+const i18n = i18next
+  .use(languageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: Env.tryGet("ENV", "development") === "development",
+    fallbackLng: "en",
+    supportedLngs: ["en", "en-US"],
+    ns: ["translation"],
+    resources
+  });
+
+export default i18n;
---
+++ template/src/core/i18n/polyfills.ts
@@ -0,0 +1,17 @@
+/**
+ * ProjectName
+ *
+ * Intl Polyfills
+ *
+ * Author: Phia Nasol
+ *
+ */
+
+import "@formatjs/intl-getcanonicallocales/polyfill";
+import "@formatjs/intl-locale/polyfill";
+import "@formatjs/intl-pluralrules/polyfill";
+import "@formatjs/intl-pluralrules/locale-data/en";
+import "@formatjs/intl-relativetimeformat/polyfill";
+import "@formatjs/intl-relativetimeformat/locale-data/en";
+import "@formatjs/intl-listformat/polyfill";
+import "@formatjs/intl-listformat/locale-data/en";
