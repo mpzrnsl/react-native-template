01404cb7a844070138c3f1c604418bfc9e9b8122
Phia Nasol <mpzrnsl@outlook.com>
2023-02-12 15:21:21 +0800

    Configure linking on iOS

--- template/ios/ProjectName/AppDelegate.mm
+++ template/ios/ProjectName/AppDelegate.mm
@@ -2,6 +2,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <RNFSManager.h>
+#import <React/RCTLinkingManager.h>
 
 @implementation AppDelegate
 
@@ -38,6 +39,20 @@
 - (void)application:(UIApplication *)application handleEventsForBackgroundURLSession:(NSString *)identifier completionHandler:(void (^)())completionHandler
 {
   [RNFSManager setCompletionHandlerForIdentifier:identifier completionHandler:completionHandler];
+}
+
+// react-navigation
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
 }
 
 @end
